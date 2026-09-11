ad16b4eaac7de95864af4b40c7843c28b6b0fe76
limitless <limitless0163@gmail.com>
2026-09-11 20:44:13 +0800

    day02_15

---
+++ day02/15_class类-成员可见性private.ts
@@ -0,0 +1,26 @@
+// 父类
+class Animal {
+  private __run__() {
+    console.log("Animal 内部辅助函数");
+  }
+
+  // 受保护的
+  protected move() {
+    this.__run__();
+    console.log("走两步");
+  }
+
+  // 公开的
+  run() {
+    this.__run__();
+    this.move();
+    console.log("跑起来");
+  }
+}
+
+// 子类
+class Dog extends Animal {
+  bark() {
+    console.log("旺旺！");
+  }
+}
